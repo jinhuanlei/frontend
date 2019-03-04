b52e77012cc40f6e8a4e8e7d5e6b9a782772239a
JinhuanLei <lei.jinhuan@uwlax.edu>
2019-03-04 11:56:40 -0600

    optimise code of datasource, add delete model

--- src/app/model-list/model-list.component.ts
+++ src/app/model-list/model-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Router} from '@angular/router';
+import {MatTableDataSource} from '@angular/material';
 
 export interface PeriodicElement {
   id: string;
@@ -17,17 +18,20 @@ export interface PeriodicElement {
 })
 export class ModelListComponent implements OnInit {
   displayedColumns: string[] = ['modelId', 'modelName', 'modelPeriod', 'operation'];
-  myDataArray: any;
+  myDataArray = new MatTableDataSource<Model>();
   baseUrl: string = environment.apiUrl;
-
+  modelData : any;
   constructor(private http: HttpClient, private router: Router) {
   }
 
 
   ngOnInit() {
     this.http.get(this.baseUrl + 'visual/models/').subscribe(
-      data => {
-        this.myDataArray = data;
+      (data : Model[]) => {
+        // this.myDataArray = data;
+        this.myDataArray = new MatTableDataSource<Model>(data);
+        this.modelData = data;
+        console.log(data);
       });
   }
 
@@ -36,4 +40,28 @@ export class ModelListComponent implements OnInit {
     this.router.navigateByUrl('modelitem');
   }
 
+  deleteModel(obj) {
+    console.log(obj);
+    this.http.get(this.baseUrl + 'visual/model/delete/' + obj.id + '/').subscribe(
+      (data) => {
+        console.log(data);
+        for (let x = 0; x < this.modelData.length; x++) {
+          if (this.modelData[x].id == Number(data)) {
+            console.log('delete');
+            this.modelData.splice(x, 1);
+            this.myDataArray = new MatTableDataSource<Model>(this.modelData);
+          }
+        }
+      },
+      error => {
+        console.log('delete fails');
+      }
+    );
+  }
+
+}
+export interface Model {
+  id: string;
+  model_name: string;
+  model_duration : string;
 }
